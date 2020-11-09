365e770178d84888ca4f785f060518b214e3c575
magistrfox <magistrfox.business@gmail.com>
2020-11-10 01:26:08 +0300

    fix connection string

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.URI
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = {
