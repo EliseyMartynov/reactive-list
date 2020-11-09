9a965ea236a6d62983655ae97bd76597399c75de
magistrfox <magistrfox.business@gmail.com>
2020-11-10 01:16:05 +0300

    traversy media deployment

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const errorMiddleware = require('./middleware/errorMiddleware')
 const todosRoute = require("./routes/todosRoute");
 const db = require("./db");
+const path = require('path')
+const fs = require('fs')
 
 
 dotenv.config();
@@ -26,6 +28,12 @@ app.use(express.json());
 app.use("/api/todos", todosRoute);
 
 app.use(errorMiddleware);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+  app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000;
 
